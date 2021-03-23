03702c33bdc65218db63629f3a4be3c7db578924
Bartosz Klonowski <Bartosz.Klonowski@callstack.com>
2021-03-23 17:07:31 +0100

    Create pages for each of the Application component
    The architecture of the whole application system is based on the
    navigation between separate components implemented in the React Native.
    These components are to be rendered as the UWP's Page content, just like
    in the original RNW application.
    The difference is, that in this approach each RN Component will be
    rendered inside separate Page as it's own ReactNative Root View.
    
    This approach requires all Components to have the separate Page
    implemented, each containing XAML scheme, IDL implementation and it's
    code-behind implemented in native C++.

---
+++ windows/ReactNativeNotes/NotesPage.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "NotesPage.h"
+#include "NotesPage.g.cpp"
+
+#include "App.h"
+
+
+namespace winrt::ReactNativeNotes::implementation
+{
+    NotesPage::NotesPage()
+    {
+        InitializeComponent();
+        auto app = Windows::UI::Xaml::Application::Current().as<App>();
+        ReactRootView().ReactNativeHost( app->Host() );
+    }
+}
---
+++ windows/ReactNativeNotes/UserAccountPage.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "UserAccountPage.h"
+#include "UserAccountPage.g.cpp"
+
+#include "App.h"
+
+
+namespace winrt::ReactNativeNotes::implementation
+{
+    UserAccountPage::UserAccountPage()
+    {
+        InitializeComponent();
+        auto app = Windows::UI::Xaml::Application::Current().as<App>();
+        ReactRootView().ReactNativeHost( app->Host() );
+    }
+}
