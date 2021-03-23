e91f41cb49352502c98e5d4dfa8428f8ba05a4aa
Bartosz Klonowski <Bartosz.Klonowski@callstack.com>
2021-03-23 09:52:11 +0100

    Make the App a main layout
    The main layout of the application is divided into sections, each
    containing crucial options and features.
    The main App class component should be responsible only for calling
    widgets provided by separate components' classes.

--- App.js
+++ App.js
@@ -1,7 +1,4 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
  * @format
  * @flow strict-local
  */
@@ -9,58 +6,25 @@
 import React from 'react';
 import {
   StyleSheet,
-  ScrollView,
   View,
-  Text,
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
 
 
 class App extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.scrollView}>
-        <Header/>
-        <View style={styles.body}>
-          <View>
-            <Text style={styles.sectionTitle}>Learn More</Text>
-            <Text style={styles.sectionDescription}>
-              Read the docs to discover what to do next:
-            </Text>
-          </View>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View style={styles.mainLayout}>
+      </View>
     );
   }
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  mainLayout: {
+    flex: 1,
+    flexDirection: "row",
   },
-  body: {
-    backgroundColor: Colors.lighter,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    paddingHorizontal: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    paddingHorizontal: 24,
-    fontWeight: '400',
-    color: Colors.dark,
-  }
 });
 
 export default App;
