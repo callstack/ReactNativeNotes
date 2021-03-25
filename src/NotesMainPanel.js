e787438553b0d3a95c5e7282890acc2be24346c2
Bartosz Klonowski <Bartosz.Klonowski@callstack.com>
2021-03-25 12:02:13 +0100

    Create main panel for notes
    The application's main panel contains all the notes added by the user.
    They are displayed in the grid, with number of columns adapted to the
    width of the window.
    Note, that the Window size needs to be considered instead of Screen's.
    That is due to having the Page in RNW rendered as a root with certain
    and fixed dimensions.
    
    Currently only the testing data are given to the FlatList, so there's a
    possibility to test it by displaying any content.
    In further development that data should be taken from the database.

--- src/NotesMainPanel.js
+++ src/NotesMainPanel.js
@@ -6,18 +6,49 @@
 import React from 'react';
 import {
   AppRegistry,
+  Dimensions,
+  FlatList,
   StyleSheet,
-  Text,
   View,
 } from 'react-native';
+import NoteWidget from './Widgets/NoteWidget';
+
+const window = Dimensions.get("window");
+const screen = Dimensions.get("screen");
+
+const noteWidgetWidth = 400;
 
 
 class NotesMainPanel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: [{key: "1"}, {key: "2"}, {key: "3"}, {key: "4"}, {key: "5"}, {key: "6"}, {key: "7"}, {key: "8"}, {key: "9"}],
+      dimensions: {window, screen},
+      columns: 1,
+    }
+  };
+
+  onChange = ({ window, screen }) => {
+    this.setState({ dimensions: { window, screen }, columns: Math.floor(window.width / noteWidgetWidth) });
+  };
+
+  componentDidMount() {
+    Dimensions.addEventListener("change", this.onChange);
+  };
+
+  componentWillUnmount() {
+    Dimensions.removeEventListener("change", this.onChange);
+  };
+
+  renderNote = notes => {
+    return <NoteWidget width={noteWidgetWidth} ID={notes.item.key}/>
+  };
 
   render() {
     return(
-      <View>
-        <Text>NotesMainPanel</Text>
+      <View style={styles.mainContainer}>
+        <FlatList numColumns={this.state.columns} key={this.state.columns} data={this.state.notes} renderItem={this.renderNote}/>
       </View>
     );
   }
@@ -25,6 +56,12 @@ class NotesMainPanel extends React.Component {
 
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    flexDirection: "column",
+    margin: 20,
+    backgroundColor: "transparent",
+  },
 });
 
 
