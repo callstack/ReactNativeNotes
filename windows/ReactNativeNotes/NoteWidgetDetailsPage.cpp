9e57b04c1d270704cf965214bdfa00169f5ac0fe
Bartosz Klonowski <Bartosz.Klonowski@callstack.com>
2021-03-25 15:37:58 +0100

    Add new page for Note details screen

---
+++ windows/ReactNativeNotes/NoteWidgetDetailsPage.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "NoteWidgetDetailsPage.h"
+#include "NoteWidgetDetailsPage.g.cpp"
+
+#include "App.h"
+
+
+namespace winrt::ReactNativeNotes::implementation
+{
+    NoteWidgetDetailsPage::NoteWidgetDetailsPage()
+    {
+        InitializeComponent();
+        auto app = Windows::UI::Xaml::Application::Current().as<App>();
+        ReactRootView().ReactNativeHost( app->Host() );
+    }
+}
---
+++ windows/ReactNativeNotes/NoteWidgetDetailsPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "NoteWidgetDetailsPage.g.h"
+
+namespace winrt::ReactNativeNotes::implementation
+{
+    class NoteWidgetDetailsPage : public NoteWidgetDetailsPageT<NoteWidgetDetailsPage>
+    {
+    public:
+        NoteWidgetDetailsPage();
+    };
+}
+
+namespace winrt::ReactNativeNotes::factory_implementation
+{
+    class NoteWidgetDetailsPage : public NoteWidgetDetailsPageT<NoteWidgetDetailsPage, implementation::NoteWidgetDetailsPage>
+    {
+    };
+}
