6ba39f9f44c594bca399e0e2c0be1cc52f5e22b7
Bartosz Klonowski <Bartosz.Klonowski@callstack.com>
2021-04-07 09:45:57 +0200

    Remove unused files

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
 
 import NotesMainPanel from './src/NotesMainPanel';
 import UserAccountPanel from './src/UserAccountPanel';
@@ -12,4 +10,3 @@ import ApplicationSettingsPanel from './src/ApplicationSettingsPanel';
 import NoteWidgetDetailsPanel from './src/NoteWidgetDetailsPanel';
 import CreateNotePanel from './src/CreateNotePanel';
 
-AppRegistry.registerComponent(appName, () => App);
