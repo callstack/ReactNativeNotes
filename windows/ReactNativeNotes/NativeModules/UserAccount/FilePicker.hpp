38130c38fe945bb4e8c23dcf20d767c54512e54d
Bartosz Klonowski <Bartosz.Klonowski@callstack.com>
2021-04-23 14:42:14 +0200

    Catch the exception in case of general window failure
    When trying to open the window on the separate thread (after launching
    the coroutine) it is possible that there'll be the exception thrown by
    the base.h implementation.
    The reason for specifying the winrt::hresult_error is that it does not
    inherits from the std::exception so it would be impossible to catch by
    the universal exception.

--- windows/ReactNativeNotes/NativeModules/UserAccount/FilePicker.hpp
+++ windows/ReactNativeNotes/NativeModules/UserAccount/FilePicker.hpp
@@ -17,16 +17,23 @@ namespace ReactNativeNotes
             winrt::Windows::Storage::Pickers::FileOpenPicker openPicker;
             openPicker.ViewMode( winrt::Windows::Storage::Pickers::PickerViewMode::Thumbnail );
             openPicker.FileTypeFilter().ReplaceAll( { L".jpg", L".jpeg", L".png" } );
-            winrt::Windows::Storage::StorageFile file = co_await openPicker.PickSingleFileAsync();
 
-            if( file != nullptr )
+            try
             {
-                std::string s = winrt::to_string(file.Path());
-                result.Resolve( React::JSValue( s ) );
+                winrt::Windows::Storage::StorageFile file = co_await openPicker.PickSingleFileAsync();
+                if( file != nullptr )
+                {
+                    std::string s = winrt::to_string( file.Path() );
+                    result.Resolve( React::JSValue( s ) );
+                }
+                else
+                {
+                    result.Reject( L"Couldn't load the selected file!" );
+                }
             }
-            else
+            catch( const winrt::hresult_error& e )
             {
-                result.Reject( L"Couldn't load the selected file!" );
+                result.Reject( e.message().c_str() );
             }
         }
     };
