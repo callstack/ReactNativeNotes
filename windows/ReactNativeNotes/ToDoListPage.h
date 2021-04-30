6414219b221027634998e60c5521e531237c1cf7
Bartosz Klonowski <Bartosz.Klonowski@callstack.com>
2021-04-30 12:28:32 +0200

    Add ToDoList Page to the native layer
    The UWP's Page for ToDoList is added and registered in the MainPage XAML
    implementation.
    This page is the host to the ToDoListPanel implemented on the JS side.

---
+++ windows/ReactNativeNotes/ToDoListPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "ToDoListPage.g.h"
+
+namespace winrt::ReactNativeNotes::implementation
+{
+    class ToDoListPage : public ToDoListPageT<ToDoListPage>
+    {
+    public:
+        ToDoListPage();
+    };
+}
+
+namespace winrt::ReactNativeNotes::factory_implementation
+{
+    class ToDoListPage : public ToDoListPageT<ToDoListPage, implementation::ToDoListPage>
+    {
+    };
+}
