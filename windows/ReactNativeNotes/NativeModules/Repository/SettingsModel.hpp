662dcc93cb936f9f012ce036567548acf37851f6
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2021-07-06 20:33:19 +0200

    Add Language to the database engine

--- windows/ReactNativeNotes/NativeModules/Repository/SettingsModel.hpp
+++ windows/ReactNativeNotes/NativeModules/Repository/SettingsModel.hpp
@@ -2,11 +2,19 @@
 
 namespace winrt::ReactNativeNotes::implementation
 {
+    enum class LanguageValue : int
+    {
+        English = 0,
+        Polish = 1
+    };
+
     class SettingsModel
     {
     public:
+        const LanguageValue Language();
+        void Language( const LanguageValue& lang );
 
     private:
-
+        LanguageValue lang = LanguageValue::English;
     };
 }
