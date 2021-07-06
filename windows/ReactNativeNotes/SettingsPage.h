18af548de8c93f1bf527ec0194fa8db45a2e4e64
Bartosz Klonowski <70535775+BartoszKlonowski@users.noreply.github.com>
2021-07-06 14:15:54 +0200

    Add new XAML page for settings screen

---
+++ windows/ReactNativeNotes/SettingsPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "SettingsPage.g.h"
+
+namespace winrt::ReactNativeNotes::implementation
+{
+    class SettingsPage : public SettingsPageT<SettingsPage>
+    {
+    public:
+        SettingsPage();
+    };
+}
+
+namespace winrt::ReactNativeNotes::factory_implementation
+{
+    class SettingsPage : public SettingsPageT<SettingsPage, implementation::SettingsPage>
+    {
+    };
+}
